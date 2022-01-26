8b9c9f27e6ff144e82ded8bd5cf911cef89223b1
yazalde filimone pinto <yazaldefilimon@gmail.com>
2022-01-26 13:58:02 +0200

    test: ensure LocalSavePurchases delete old cache when call sut.save

--- src/data/use-cases/local-save-purchases.spec.ts
+++ src/data/use-cases/local-save-purchases.spec.ts
@@ -1,13 +1,22 @@
 class LocalSavePurchases{
   constructor(private readonly  cacheStorage:CacheStorage){} 
+
+  save(): Promise<void>{
+    this.cacheStorage.delete();
+  }
 }
 
 interface CacheStorage{
+  delete: () => void
 
 }
 
 class CacheStorageSpy implements CacheStorage{
   deleteCallsCount = 0;
+
+  async delete():void{
+    this.deleteCallsCount++
+  }
 }
 
 
@@ -17,4 +26,12 @@ describe("LocalSavePurchases", () => {
     new LocalSavePurchases(cacheStorage);
     expect(cacheStorage.deleteCallsCount).toBe(0);
   }) 
-})
+
+  it('should delete old cache when call sut.save', async () => {
+    const cacheStorage = new CacheStorageSpy();
+    const sut = new LocalSavePurchases(cacheStorage);
+    await sut.save();
+
+    expect(cacheStorage.deleteCallsCount).toBe(1);
+  })
+}
